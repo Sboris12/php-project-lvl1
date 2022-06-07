3b6feaf16075871f1440da1e29045e3901d06de7
Sboris12 <105364457+Sboris12@users.noreply.github.com>
2022-06-07 13:39:53 +0300

    Step 4

--- src/Cli.php
+++ src/Cli.php
@@ -2,22 +2,12 @@
 
 namespace src\Cli;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 use function cli\line;
 use function cli\prompt;
 
-
-
 function hellow($name)
 {
-   line('Welcome to the Brain Game!');
-   $name = prompt('May I have your name?');
-   line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
